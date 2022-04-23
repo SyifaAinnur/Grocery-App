6fbe46f7fc65be2e120fcaecac4ae9e9000bff58
Syifa Ainnur <arwahitem01@gmail.com>
2022-04-23 15:28:23 +0700

    update login_screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,12 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:grocery_app/common_widgets/app_text.dart';
 import 'package:intl_phone_field/countries.dart';
 import 'package:intl_phone_field/intl_phone_field.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:grocery_app/styles/colors.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({ Key? key }) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +17,177 @@ class LoginScreen extends StatelessWidget {
       body: SafeArea(
         bottom: false,
         child: Stack(
-          alignment: Alignment.topCenter,
           children: [
             Container(
-              height: 300.0,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/images/banner_background.png"),
-                  fit: BoxFit.cover,
+                height: double.infinity,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomRight,
+                        colors: [
+                      Color(0xff94CAA6),
+                      Color(0xffF5F4ED),
+                    ])),
+                child: Align(
+                  alignment: Alignment.topRight,
+                  child: Image.asset(
+                    'assets/images/Mask_Group.png',
+                  ),
+                )),
+            ListView(
+              children: [
+                SizedBox(
+                  height: 300,
                 ),
-              ),
-              ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(top: Radius.circular(20)),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20),
+                        child: AppText(
+                          text: "Get your groceries\n with nectar",
+                          textAlign: TextAlign.start,
+                          fontSize: 26,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: IntlPhoneField(
+                          decoration:
+                              InputDecoration(labelText: 'Phone Number'),
+                          onChanged: (phone) {
+                            print(phone.completeNumber);
+                          },
+                          onCountryChanged: (country) {
+                            print('Country code changed to: ' + country.name);
+                          },
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Center(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 20),
+                          child: AppText(
+                            text: "Or connect with social media",
+                            textAlign: TextAlign.center,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      GoogleButton(),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      FacebookButton(),
+                      SizedBox(
+                        height: 60
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget GoogleButton() {
+    return Container(
+      width: double.maxFinite,
+      margin: EdgeInsets.symmetric(horizontal: 25),
+      child: RaisedButton(
+        visualDensity: VisualDensity.compact,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+        ),
+        color: Color(0xff5383EC),
+        textColor: Colors.white,
+        elevation: 0.0,
+        padding: EdgeInsets.symmetric(vertical: 24, horizontal: 25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 20,
+              height: 20,
+              child: SvgPicture.asset(
+                "assets/icons/Google_icon.svg",
+              ),
+            ),
+            Text(
+              "Continue with Google",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Container()
+          ],
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+    Widget FacebookButton() {
+    return Container(
+      width: double.maxFinite,
+      margin: EdgeInsets.symmetric(horizontal: 25),
+      child: RaisedButton(
+        visualDensity: VisualDensity.compact,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+        ),
+        color: Color(0xff4A66AC),
+        textColor: Colors.white,
+        elevation: 0.0,
+        padding: EdgeInsets.symmetric(vertical: 24, horizontal: 25),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 20,
+              height: 20,
+              child: SvgPicture.asset(
+                "assets/icons/Facebook.svg",
+              ),
+            ),
+            Text(
+              "Continue with Facebook",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+            Container()
+          ],
+        ),
+        onPressed: () {},
       ),
     );
   }
