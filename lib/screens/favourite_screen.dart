3a7d8e0a14273f5fdf94aa9577f54354a6606528
Syifa Ainnur <arwahitem01@gmail.com>
2022-04-23 13:31:40 +0700

    Update favourite_screen.dart

--- lib/screens/favourite_screen.dart
+++ lib/screens/favourite_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/common_widgets/app_text.dart';
 
 class FavouriteScreen extends StatelessWidget {
   const FavouriteScreen({ Key? key }) : super(key: key);
@@ -6,7 +7,14 @@ class FavouriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Center(
+        child: AppText(
+          text: "No Favorite Items",
+          fontWeight: FontWeight.w600,
+          color: Color(0xFF7C7C7C),
+          textAlign: TextAlign.start,
+        ),
+      ),
     );
   }
 }
