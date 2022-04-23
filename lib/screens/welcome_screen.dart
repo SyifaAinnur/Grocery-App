cd6f670c961ce947ea5c185d05c89995df517019
Syifa Ainnur <arwahitem01@gmail.com>
2022-04-23 19:14:57 +0700

    update signin

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:grocery_app/common_widgets/app_button.dart';
 import 'package:grocery_app/common_widgets/app_text.dart';
-import 'package:grocery_app/screens/Sing%20in/SignIn_screen.dart';
+import 'package:grocery_app/screens/SingIn/SignIn_screen.dart';
 import 'package:grocery_app/screens/dashboard/dashboard_screen.dart';
 import 'package:grocery_app/styles/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
