fd96df386ce94f5d0654b255012c26a3961dff10
Syifa Ainnur <arwahitem01@gmail.com>
2022-04-23 13:33:57 +0700

    Update account_screen.dart

--- lib/screens/account/account_screen.dart
+++ lib/screens/account/account_screen.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:grocery_app/common_widgets/app_text.dart';
+import 'package:grocery_app/helpers/column_with_seprator.dart';
+import 'package:grocery_app/styles/colors.dart';
 
 class AccountScreen extends StatelessWidget {
   const AccountScreen({ Key? key }) : super(key: key);
